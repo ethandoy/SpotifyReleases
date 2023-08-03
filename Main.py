f3e8c89bfcc4fed1d6fbeb8ceda2366412e3cd0f
ethandoy <ethanvdoyle@icloud.com>
2023-08-02 20:37:48 -0400

    compare release date vs today

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 from requests import post, get
 import json
-from datetime import datetime
+from datetime import date, datetime
 import auth
 
 
@@ -24,8 +24,20 @@ if __name__ == "__main__":
     for key in artists.keys():
         albums = get_artists_albums(token, artists[key])
         album_list = albums['items']
-        for idx, alb in enumerate(album_list):
-            print(f"{idx+1}. {key},{alb['name']} - {alb['release_date']}")
+        for album in album_list:
+            try:
+                album_date = datetime.strptime(album['release_date'], "%Y-%m-%d").date()
+                if album_date == date.today():
+                    print(f"New Release from {key}! Album: {album['name']}")
+            except:
+                try:
+                    album_date = datetime.strptime(album['release_date'], "%Y").date()
+                except:
+                    print(f'--date error--\nfor:{key}\ngot:{album["release_date"]}\n--------------')
+                
+
+        # for idx, alb in enumerate(album_list):
+        #     print(f"{idx+1}. {key},{alb['name']} - {alb['release_date']}")
     # with open('res.json', 'w') as f:
     #     json.dump(albums, f, indent=4)
 
